6cf9c7a495eb483279ccf1987325fa1060d6dfa3
sharad <sharad.s@sudhaya.com>
2018-06-25 11:35:28 +0530

    2

--- public/script.js
+++ public/script.js
@@ -1,8 +1,35 @@
-fetch('/temperature')
-.then(results => {
+const fetchTemperature = () => {
+    fetch('/temperature')
+    .then(results => {
+    
+        return results.text()
+    })
+    .then(text => {
+        //console.log(text)
+        const temperatureDispaly = document.getElementById('temperature-display')
+        temperatureDispaly.innerHTML = text
+    })
 
-    return results.text()
-})
-.then(text => {
-    console.log(text)
-})
+}
+
+const fetchHumidity = () => {
+    fetch('/humidity')
+    .then(results => {
+        return results.text()
+    })
+    .then(text =>{
+        const temperatureDispaly = document.getElementById('humidity-display')
+        temperatureDispaly.innerHTML = text 
+    })
+}
+
+setInterval(() =>{
+    fetchTemperature()
+    fetchHumidity()
+},2000)
+
+
+
+
+
+
